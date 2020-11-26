764c8c09b2657e015c6ab7f4a694bdc3c2d30217
dondakeshimo <went.went.takkun135@gmail.com>
2020-11-26 09:58:27 +0900

    feat: add setReminder

--- internal/entities/task/task.go
+++ internal/entities/task/task.go
@@ -1,9 +1,36 @@
 package task
 
+import (
+	"fmt"
+
+	"github.com/dondakeshimo/todo-cli/pkg/scheduler"
+	"github.com/dondakeshimo/todo-cli/internal/entities/timestr"
+)
+
 type Task struct {
 	ID         int    `json:"id"`
 	Task       string `json:"task"`
 	RemindTime string `json:"remindtime"`
 	UUID       string `json:"uuid"`
 	Reminder   string `json:"reminder"`
+}
+
+func (t *Task) SetReminder() error {
+	ts, err := timestr.Parse(t.RemindTime)
+	if err != nil {
+		return err
+	}
+
+	sr := &scheduler.Request{
+		DateTime: *ts,
+		Command: fmt.Sprintf("todo notify --uuid %s", t.UUID),
+	}
+
+	s := scheduler.NewLaunchdScheduler()
+
+	if err := s.Register(sr); err != nil {
+		return err
+	}
+
+	return err
 }
