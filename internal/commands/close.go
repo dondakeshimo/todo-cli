41dbcfe7f470e97512dd41378461c2b9287c94f6
dondakeshimo <went.went.takkun135@gmail.com>
2020-12-02 00:35:36 +0900

    feat: close reminder and task at the same time

--- internal/commands/close.go
+++ internal/commands/close.go
@@ -1,7 +1,10 @@
 package commands
 
 import (
+	"runtime"
+
 	"github.com/dondakeshimo/todo-cli/internal/entities/task"
+	"github.com/dondakeshimo/todo-cli/pkg/scheduler"
 	"github.com/urfave/cli/v2"
 )
 
@@ -12,6 +15,23 @@ func Close(c *cli.Context) error {
 	}
 
 	ids := c.IntSlice("ids")
+
+	for _, i := range ids {
+		t := h.GetTask(i)
+		if t.Reminder == "" {
+			continue
+		}
+
+		var s scheduler.Scheduler
+		if runtime.GOOS == "darwin" {
+			s = scheduler.NewLaunchdScheduler()
+		} else {
+			continue
+		}
+
+		s.RemoveWithID(t.UUID)
+	}
+
 	h.RemoveTasks(ids)
 
 	if err := h.Write(); err != nil {
