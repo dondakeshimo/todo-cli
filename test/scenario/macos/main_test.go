4ffa09eed860376b1e099745b6f8756745fe96f2
dondakeshimo <went.went.takkun135@gmail.com>
2020-12-05 17:05:15 +0900

    test: manage todo.json

--- test/scenario/macos/main_test.go
+++ test/scenario/macos/main_test.go
@@ -2,12 +2,13 @@ package scenario_test
 
 import (
 	"fmt"
-	"testing"
 	"os"
+	"path/filepath"
+	"testing"
 )
 
 func TestMain(m *testing.M) {
-	cleanup, err := setEnv()
+	cleanup, err := setup()
 	if err != nil {
 		fmt.Printf("could not set xdg for test: %s", err.Error())
 	}
@@ -17,8 +18,11 @@ func TestMain(m *testing.M) {
 
 }
 
-func setEnv() (func(), error) {
+func setup() (func(), error) {
 	const varName = "XDG_DATA_HOME"
+	const jsonFilename = "todo.json"
+	const jsonDir = "todo"
+
 	xdg := os.Getenv(varName)
 
 	dir, err := os.Getwd()
@@ -30,7 +34,16 @@ func setEnv() (func(), error) {
 		return nil, err
 	}
 
-	return func () {
+	return func() {
+		p := filepath.Join(dir, jsonDir, jsonFilename)
+		_, err = os.Stat(p)
+		if err == nil || os.IsExist(err) {
+			fmt.Printf("remove %s", p)
+			if err := os.Remove(p); err != nil {
+				fmt.Printf("could not remove [%s]: %s", p, err.Error())
+			}
+		}
+
 		if err := os.Setenv(varName, xdg); err != nil {
 			fmt.Printf("could not set original xdg, please check your xdg home.: %s", err.Error())
 		}
