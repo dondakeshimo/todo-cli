c499c05d84a59eb6527b315ef7ad3a5b35715ee0
dondakeshimo <went.went.takkun135@gmail.com>
2021-05-16 12:23:43 +0900

    refactor: separate usecase list

---
+++ internal/usecases/list.go
@@ -0,0 +1,36 @@
+package usecases
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/dondakeshimo/todo-cli/internal/entities/task"
+	"github.com/dondakeshimo/todo-cli/internal/gateways/json"
+	"github.com/olekukonko/tablewriter"
+)
+
+// List is a function that show task list.
+func List() error {
+	jc, err := json.NewClient()
+	if err != nil {
+		return err
+	}
+
+	h, err := task.NewHandler(jc)
+	if err != nil {
+		return err
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetAutoFormatHeaders(false) // for preventing header from being made upper case automatically
+
+	table.SetHeader([]string{"ID", "Task", "RemindTime", "Reminder", "Priority"})
+
+	for _, t := range h.GetTasks() {
+		table.Append([]string{strconv.Itoa(t.ID()), t.Task(), string(t.RemindTime()), string(t.Reminder()), strconv.Itoa(t.Priority())})
+	}
+
+	table.Render()
+
+	return nil
+}
