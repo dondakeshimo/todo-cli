b514a6f5473673c3a65cdc95c03dbaa29b25a57f
dondakeshimo <went.went.takkun135@gmail.com>
2021-05-16 12:07:52 +0900

    refactor: separate usecase close

---
+++ internal/usecases/close.go
@@ -0,0 +1,52 @@
+package usecases
+
+import (
+	"github.com/dondakeshimo/todo-cli/internal/entities/task"
+	"github.com/dondakeshimo/todo-cli/internal/gateways/json"
+	"github.com/dondakeshimo/todo-cli/pkg/scheduler"
+)
+
+// Close is a function that close a task or tasks.
+func Close(ids []int) error {
+	jc, err := json.NewClient()
+	if err != nil {
+		return err
+	}
+
+	h, err := task.NewHandler(jc)
+	if err != nil {
+		return err
+	}
+
+	for _, id := range ids {
+		t, err := h.GetTask(id)
+		if err != nil {
+			return err
+		}
+
+		if t.Reminder() == "" {
+			continue
+		}
+
+		// NOTE: ignore err message
+		s, err := scheduler.NewScheduler()
+		if err != nil {
+			continue
+		}
+
+		// NOTE: ignore err message
+		if err := t.RemoveReminder(s); err != nil {
+			continue
+		}
+	}
+
+	if err := h.RemoveTasks(ids); err != nil {
+		return err
+	}
+
+	if err := h.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
