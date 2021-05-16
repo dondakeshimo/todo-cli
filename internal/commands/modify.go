e1a14b0e0f3096b8245a43f59d9cfaddd7d3d8a0
dondakeshimo <went.went.takkun135@gmail.com>
2021-05-16 12:17:26 +0900

    refactor: separate usecase modify

--- internal/commands/modify.go
+++ internal/commands/modify.go
@@ -5,175 +5,67 @@ import (
 
 	"github.com/dondakeshimo/todo-cli/internal/entities/reminder"
 	"github.com/dondakeshimo/todo-cli/internal/entities/remindtime"
-	"github.com/dondakeshimo/todo-cli/internal/entities/task"
-	"github.com/dondakeshimo/todo-cli/internal/gateways/json"
-	"github.com/dondakeshimo/todo-cli/pkg/scheduler"
+	"github.com/dondakeshimo/todo-cli/internal/usecases"
 	"github.com/urfave/cli/v2"
 )
 
-type modifyParams struct {
-	id               int
-	task             string
-	isTask           bool
-	remindTime       remindtime.RemindTime
-	isRemindTime     bool
-	relativeTime     remindtime.RelativeTime
-	isRelativeTime   bool
-	isRemoveReminder bool
-	reminder         reminder.Reminder
-	isReminder       bool
-	priority         int
-}
+func Modify(c *cli.Context) error {
+    var r usecases.ModifyRequest
 
-func newModifyParams(c *cli.Context) (*modifyParams, error) {
-	p := new(modifyParams)
+	r.Id = c.Int("id") // required
 
-	p.id = c.Int("id") // required
-
-	p.task = c.String("task")
-	p.isTask = p.task != ""
+	r.Task = c.String("task")
+	r.IsTask = r.Task != ""
 
 	crt := c.String("remind_time")
 	if crt == "" {
-		p.isRemindTime = false
-		p.isRelativeTime = false
+		r.IsRemindTime = false
+		r.IsRelativeTime = false
 	}
 
 	if crt != "" && remindtime.IsValidRelativeTime(crt) {
 		td, err := remindtime.NewRelativeTime(crt)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		p.relativeTime = td
-		p.isRelativeTime = true
-		p.isRemindTime = false
+		r.RelativeTime = td
+		r.IsRelativeTime = true
+		r.IsRemindTime = false
 	}
 
 	if crt != "" && !remindtime.IsValidRelativeTime(crt) {
 		rt, err := remindtime.NewRemindTime(crt)
 		if err != nil {
-			return nil, err
+			return err
 		}
-		p.remindTime = rt
-		p.isRemindTime = true
-		p.isRelativeTime = false
+		r.RemindTime = rt
+		r.IsRemindTime = true
+		r.IsRelativeTime = false
 	}
 
 	// NOTE: assert isRelativeTime and isRemindTime never be true
-	if p.isRelativeTime && p.isRemindTime {
-		return nil, fmt.Errorf("internal command error")
+	if r.IsRelativeTime && r.IsRemindTime {
+		return fmt.Errorf("internal command error")
 	}
 
-	p.isRemoveReminder = false
+	r.IsRemoveReminder = false
 	if c.Bool("remove_reminder") {
-		p.isRemoveReminder = true
+		r.IsRemoveReminder = true
 	}
 
-	p.isReminder = false
-	if !p.isRemoveReminder && c.String("reminder") != "" {
+	r.IsReminder = false
+	if !r.IsRemoveReminder && c.String("reminder") != "" {
 		rm, err := reminder.NewReminder(c.String("reminder"))
 		if err != nil {
-			return nil, err
-		}
-		p.reminder = rm
-		p.isReminder = true
-	}
-
-	if c.IsSet("priority") {
-		p.priority = c.Int("priority")
-	}
-
-	return p, nil
-}
-
-// Modify is a function that modify a task.
-func Modify(c *cli.Context) error {
-	jc, err := json.NewClient()
-	if err != nil {
-		return err
-	}
-
-	h, err := task.NewHandler(jc)
-	if err != nil {
-		return err
-	}
-
-	p, err := newModifyParams(c)
-	if err != nil {
-		return err
-	}
-
-	t, err := h.GetTask(p.id)
-	if err != nil {
-		return err
-	}
-
-	newTask := t.Task()
-	if p.isTask {
-		newTask = p.task
-	}
-
-	newRemindTime := t.RemindTime()
-	if p.isRelativeTime {
-		nrt, err := t.RemindTime().AddTime(p.relativeTime)
-		if err != nil {
 			return err
 		}
-		newRemindTime = nrt
+		r.Reminder = rm
+		r.IsReminder = true
 	}
 
-	if p.isRemindTime {
-		newRemindTime = p.remindTime
-	}
-
-	newReminder := t.Reminder()
-
-	// construct scheduler when remove previous reminder or update reminder
-	var s scheduler.Scheduler
-	if (p.isRemoveReminder && t.Reminder() != "") || (!p.isRemoveReminder && p.isReminder) {
-		var err error
-		s, err = scheduler.NewScheduler()
-		if err != nil {
-			return err
-		}
-	}
-
-	if p.isRemoveReminder && t.Reminder() != "" {
-		if err := t.RemoveReminder(s); err != nil {
-			fmt.Println("reminder had been removed for some reason.")
-		}
-		newReminder = ""
-	}
-
-	if !p.isRemoveReminder && p.isReminder {
-		if t.Reminder() != "" {
-			if err := t.RemoveReminder(s); err != nil {
-				fmt.Println("previous reminder had been removed for some reason.")
-			}
-		}
-
-		newReminder = p.reminder
-	}
-
-	newPriority := t.Priority()
-	if c.IsSet("priority") {
-		newPriority = p.priority
-	}
-
-	nt := task.NewTask(t.ID(), newTask, newRemindTime, t.UUID(), newReminder, newPriority)
-
-	if !p.isRemoveReminder && p.isReminder {
-		if err := nt.SetReminder(s); err != nil {
-			return err
-		}
-	}
-
-	if err := h.UpdateTask(t.ID(), nt); err != nil {
-		return err
-	}
-
-	if err := h.Commit(); err != nil {
-		return err
+    r.IsPriority = c.IsSet("priority")
+	if r.IsPriority {
+		r.Priority = c.Int("priority")
 	}
 
 	return nil
---
+++ internal/usecases/modify.go
@@ -0,0 +1,118 @@
+package usecases
+
+import (
+	"fmt"
+
+	"github.com/dondakeshimo/todo-cli/internal/entities/reminder"
+	"github.com/dondakeshimo/todo-cli/internal/entities/remindtime"
+	"github.com/dondakeshimo/todo-cli/internal/entities/task"
+	"github.com/dondakeshimo/todo-cli/internal/gateways/json"
+	"github.com/dondakeshimo/todo-cli/pkg/scheduler"
+)
+
+type ModifyRequest struct {
+	Id               int
+	Task             string
+	IsTask           bool
+	RemindTime       remindtime.RemindTime
+	IsRemindTime     bool
+	RelativeTime     remindtime.RelativeTime
+	IsRelativeTime   bool
+	IsRemoveReminder bool
+	Reminder         reminder.Reminder
+	IsReminder       bool
+	Priority         int
+	IsPriority       bool
+}
+
+// Modify is a function that modify a task.
+func Modify(r ModifyRequest) error {
+	jc, err := json.NewClient()
+	if err != nil {
+		return err
+	}
+
+	h, err := task.NewHandler(jc)
+	if err != nil {
+		return err
+	}
+
+	if err != nil {
+		return err
+	}
+
+	t, err := h.GetTask(r.Id)
+	if err != nil {
+		return err
+	}
+
+	newTask := t.Task()
+	if r.IsTask {
+		newTask = r.Task
+	}
+
+	newRemindTime := t.RemindTime()
+	if r.IsRelativeTime {
+		nrt, err := t.RemindTime().AddTime(r.RelativeTime)
+		if err != nil {
+			return err
+		}
+		newRemindTime = nrt
+	}
+
+	if r.IsRemindTime {
+		newRemindTime = r.RemindTime
+	}
+
+	newReminder := t.Reminder()
+
+	// construct scheduler when remove previous reminder or update reminder
+	var s scheduler.Scheduler
+	if (r.IsRemoveReminder && t.Reminder() != "") || (!r.IsRemoveReminder && r.IsReminder) {
+		var err error
+		s, err = scheduler.NewScheduler()
+		if err != nil {
+			return err
+		}
+	}
+
+	if r.IsRemoveReminder && t.Reminder() != "" {
+		if err := t.RemoveReminder(s); err != nil {
+			fmt.Println("reminder had been removed for some reason.")
+		}
+		newReminder = ""
+	}
+
+	if !r.IsRemoveReminder && r.IsReminder {
+		if t.Reminder() != "" {
+			if err := t.RemoveReminder(s); err != nil {
+				fmt.Println("previous reminder had been removed for some reason.")
+			}
+		}
+
+		newReminder = r.Reminder
+	}
+
+	newPriority := t.Priority()
+	if r.IsPriority {
+		newPriority = r.Priority
+	}
+
+	nt := task.NewTask(t.ID(), newTask, newRemindTime, t.UUID(), newReminder, newPriority)
+
+	if !r.IsRemoveReminder && r.IsReminder {
+		if err := nt.SetReminder(s); err != nil {
+			return err
+		}
+	}
+
+	if err := h.UpdateTask(t.ID(), nt); err != nil {
+		return err
+	}
+
+	if err := h.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
