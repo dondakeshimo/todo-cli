57e30ac1affc1f72f09d6811a1a1bfd86ec95aea
dondakeshimo <went.went.takkun135@gmail.com>
2021-07-18 16:34:50 +0900

    feat: hide columns according to config

--- pkg/usecases/list.go
+++ pkg/usecases/list.go
@@ -8,6 +8,14 @@ import (
 	"github.com/olekukonko/tablewriter"
 )
 
+type columns struct {
+	id         string
+	task       string
+	remindTime string
+	reminder   string
+	priority   string
+}
+
 // List is a function that show task list.
 func List() error {
 	h, err := task.NewHandler(taskRepository)
@@ -18,13 +26,43 @@ func List() error {
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetAutoFormatHeaders(false) // for preventing header from being made upper case automatically
 
-	table.SetHeader([]string{"ID", "Task", "RemindTime", "Reminder", "Priority"})
+	header := columns{
+		id:         "ID",
+		task:       "Task",
+		remindTime: "RemindTime",
+		reminder:   "Reminder",
+		priority:   "Priority",
+	}
+	table.SetHeader(buildRowAccordingToConfig(header))
 
 	for _, t := range h.GetTasks() {
-		table.Append([]string{strconv.Itoa(t.ID()), t.Task(), string(t.RemindTime()), string(t.Reminder()), strconv.Itoa(t.Priority())})
+		row := columns{
+			id:         strconv.Itoa(t.ID()),
+			task:       t.Task(),
+			remindTime: string(t.RemindTime()),
+			reminder:   string(t.Reminder()),
+			priority:   strconv.Itoa(t.Priority()),
+		}
+		table.Append(buildRowAccordingToConfig(row))
 	}
 
 	table.Render()
 
 	return nil
+}
+
+// buildColumnAccordingToSetting build a row of table according to hide-setting.
+// this function define column order.
+func buildRowAccordingToConfig(row columns) []string {
+	builtRow := []string{row.id, row.task, row.remindTime}
+
+	if !config.hideReminder {
+		builtRow = append(builtRow, row.reminder)
+	}
+
+	if !config.hidePriority {
+		builtRow = append(builtRow, row.priority)
+	}
+
+	return builtRow
 }
